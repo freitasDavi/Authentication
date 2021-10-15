5f918e460c71ca809bf91f1d30dfca584916f652
Davi <davi.freitas@useall.com.br>
2021-10-15 18:09:50 -0300

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import { FormEvent, useContext, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 import styles from '../styles/Home.module.css';
+import { parseCookies } from 'nookies';
+import { withSSRGuest } from '../utils/withSSRGuest';
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState();
@@ -27,4 +29,10 @@ const Home: NextPage = () => {
   )
 }
 
-export default Home
+export default Home;
+
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {}
+  }
+})
