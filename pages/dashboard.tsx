a74f30c89292327cdc562a00affd34e12b2aa193
Davi <davi.freitas@useall.com.br>
2021-10-18 13:37:30 -0300

    logout session with brodcastChannel

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -6,7 +6,7 @@ import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
-  const { user } = useContext(AuthContext);
+  const { user, signOut } = useContext(AuthContext);
 
   useEffect(() => {
     api
@@ -17,6 +17,7 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
+      <button onClick={signOut}>SignOut</button>
       <Can permissions={['metrics.list']}>
         <div>Métricas</div>
       </Can>
