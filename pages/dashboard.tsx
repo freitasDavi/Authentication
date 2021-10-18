318c7a1a182cd1a52acef4ef4f9c15e717bc96c6
Davi <davi.freitas@useall.com.br>
2021-10-18 11:16:27 -0300

    permission component created

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,16 +1,12 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
-import { useCan } from "../hooks/useCan";
+import { Can } from '../components/can';
 import { setupAPIClient } from "../services/api";
 import { api } from "../services/apiClient";
 import { withSSRAuth } from "../utils/withSSRAuth";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
-
-  const userCanSeenMetrics = useCan({
-    roles: ['administrator', 'editor']
-  })
 
   useEffect(() => {
     api
@@ -22,7 +18,9 @@ export default function Dashboard() {
   return (
     <>
       <h1>Dashboard: {user?.email}</h1>
-      { userCanSeenMetrics && <div>Métricas</div>}
+      <Can permissions={['metrics.list']}>
+        <div>Métricas</div>
+      </Can>
     </>
   );
 }
